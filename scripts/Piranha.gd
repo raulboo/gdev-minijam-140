21c963466bb7f7683e6e8b3cb43862db2b48e185
raulboo <raulpschutz@hotmail.com>
2023-09-03 15:20:35 -0300

    Piranha 2

--- scripts/Piranha.gd
+++ scripts/Piranha.gd
@@ -8,26 +8,8 @@ const SPEED = 100
 
 func _physics_process(delta):
 	
+	var dpos = position.direction_to(player.position)
 	
-	
-	
-	
-	
-	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	velocity = dpos*SPEED
 
 	move_and_slide()
---
+++ scripts/PiranhaManager.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@export var Piranha : PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	
+	
+func spawn_piranha():
+	Piranha
+	
+
