8083ce5cae44ba6ee3cdac0e58c99935d0361e57
Caina Caldas <cainacc@hotmail.com>
2023-09-03 16:28:24 -0300

    Restart the game if player is too close to the piranha

--- scripts/Piranha.gd
+++ scripts/Piranha.gd
@@ -7,9 +7,13 @@ const SPEED = 100
 
 
 func _physics_process(delta):
-	
+	# move towards player
 	var dpos = position.direction_to(player.position)
-	
 	velocity = dpos*SPEED
-
 	move_and_slide()
+	
+	# restart the scene if player is too close
+	var distance = player.position.distance_to(position)
+	var max_distance = 15
+	if (distance < max_distance):
+		get_tree().reload_current_scene()
